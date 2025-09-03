32d150d4818c93f259ad52bcf35ec8873f8a2783
Alison G. Martins <odevalison@gmail.com>
2025-09-03 20:17:19 -0300

    fix: ensure icons are imported correctly

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,11 +1,11 @@
 import AddIcon from './add.svg?react'
 import CheckIcon from './check.svg?react'
-import SunIcon from './sun.svg?react'
 import CloudSunIcon from './cloud-sun.svg?react'
-import DetailsIcon from './details.svg.svg?react'
+import DetailsIcon from './details.svg?react'
 import HomeIcon from './home.svg?react'
 import LoaderIcon from './loader.svg?react'
 import MoonIcon from './moon.svg?react'
+import SunIcon from './sun.svg?react'
 import TasksIcon from './tasks.svg?react'
 import TrashIcon from './trash.svg?react'
 
